ff8e1803a16e87ed6cb8c50e10dd45efbaf95d8e
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 11:18:58 +0100

    Task 3 - Final Commit

--- Week5AssessmentTaskThree/Program.cs
+++ Week5AssessmentTaskThree/Program.cs
@@ -7,31 +7,43 @@ string filePath = @"/Users/ronanculkin/Documents/Software Development/Semester T
 string[] ibans = File.ReadAllLines(filePath);
 
 char[] iban = new char[22];
-string? bankCode = null;
-string? sortCode = null;
-string? accNumber = null;
+string? bankCode;
+string? sortCode;
+string? accNumber;
 
 
 // Example: IE25ULSB 996622 12345678
 // Main
-for (int i = 0; i < 2; i++)
+for (int i = 0; i < 3; i++)
 {
+    // Set vars to null for each run
+    bankCode = null;
+    sortCode = null;
+    accNumber = null;
+    
+    
     iban = ibans[i].ToCharArray();
 // Bank Code
-    for (int j = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
     {
         bankCode = bankCode + iban[j];
     }
 // Sort Code
-    for (int j = 8; i < 14; i++)
+    for (int j = 8; j < 14; j++)
     {
         sortCode += iban[j];
     }
 
 // Account Number
-    for (int j = 14; i < 22; i++)
+    for (int j = 14; j < 22; j++)
     {
         accNumber += iban[j];
     }
+
+    WriteLine($"Account: {i + 1}");
+    WriteLine($"Bank Code: {bankCode}");
+    WriteLine($"Sort Code: {sortCode}");
+    WriteLine($"Account Number: {accNumber}");
+    WriteLine("============================");
 }
 
