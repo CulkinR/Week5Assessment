c8c309a4f6af6db033fa5c2db34379a82251d199
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 10:42:54 +0100

    Task 2 - Final Commit

--- Week5AssessmentTaskTwo/Program.cs
+++ Week5AssessmentTaskTwo/Program.cs
@@ -7,10 +7,12 @@ using static System.Console;
 
 //Vars
 int menuSelect;
+int standardPrice = 50;
+int finalPrice;
 
 // Children 50%, Students 20%, OAPs 10%, Normal Members 0%. Only cater for these 4 options.
-double[] discountRates = new double[] {0.50, 0.20, 0.10, 0};
-string[] memberOptions = new string[] {"Children", "Students", "OAPS", "Normal" };
+double[] discountRates = new double[] {0.50, 0.20, 0.10};
+string[] memberOptions = new string[] {"Children", "Students", "OAPs", "Normal" };
 
 // Menu Init
 Menu memberSelect = new Menu(memberOptions);
@@ -23,3 +25,26 @@ WriteLine("Select which type of member you are");
 memberSelect.menu();
 
 Write("> ");
+menuSelect = Int32.Parse(ReadLine());
+
+switch (menuSelect)
+{
+    case 1:
+        finalPrice = (int) (standardPrice - (standardPrice * discountRates[menuSelect - 1]));
+        WriteLine($"The price for a {memberOptions[menuSelect - 1]} membership is {finalPrice}");
+        break;
+    case 2:
+        finalPrice = (int) (standardPrice - (standardPrice * discountRates[menuSelect - 1]));
+        WriteLine($"The price for a {memberOptions[menuSelect - 1]} membership is {finalPrice}");
+        break;
+    case 3:
+        finalPrice = (int) (standardPrice - (standardPrice * discountRates[menuSelect - 1]));
+        WriteLine($"The price for a {memberOptions[menuSelect - 1]} membership is {finalPrice}");
+        break;
+    case 4:
+        WriteLine($"The price for a {memberOptions[menuSelect - 1]} membership is {standardPrice}");
+        break;
+    default:
+        WriteLine("Invalid Entry - Exiting");
+        break;
+}
