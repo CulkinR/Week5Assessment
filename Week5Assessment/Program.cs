4deb848ad2d183d1cc263619579ff062c0602cd0
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 10:16:13 +0100

    Week 5 Assessment Task 1 Commit

--- Week5Assessment/Program.cs
+++ Week5Assessment/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using static System.Console;
+
+// Task 1 - Temperature Reading
+
+// Vars
+int temperature;
+
+// Main
+WriteLine("Enter the temperature to test normal range (18-25)");
+Write("> ");
+temperature = Int32.Parse(ReadLine());
+
+if (temperature >= 18 && temperature <= 22)
+{
+    WriteLine("Temperature is inside the normal range of 18-25 Degrees");
+}
+else
+{
+    WriteLine("Temperature is outside the normal range of 18-25 Degrees");
+}
