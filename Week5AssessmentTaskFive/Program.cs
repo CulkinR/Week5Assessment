bfc35f99fa84bcba0a9718bf800f50ea157d5f06
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 11:55:25 +0100

    Task 5 - Final Commit

---
+++ Week5AssessmentTaskFive/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static System.Console;
+
+// Vars
+int grade;
+int finalGrade;
+
+WriteLine("Enter grade you with to adjust by 15%");
+Write("> ");
+grade = Int32.Parse(ReadLine());
+finalGrade = gradeAdjust(grade);
+WriteLine($"Final Grade is {finalGrade}");
+
+
+// If grade + 15% is less than 100 allow to run
+int gradeAdjust(int grade)
+{
+    if (grade + 15 <= 100)
+    {
+        grade += 15;
+    }
+    else
+    {
+        WriteLine("Grade is already too high - Exiting");
+    }
+    return grade;
+}
