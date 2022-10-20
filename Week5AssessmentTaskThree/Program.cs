6c85c61ad47e2a0ad698788c117c0afe7f35f2fa
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 11:12:28 +0100

    Task 3 - Second Commit

--- Week5AssessmentTaskThree/Program.cs
+++ Week5AssessmentTaskThree/Program.cs
@@ -1,3 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using static System.Console;
+
+// Vars
+string filePath = @"/Users/ronanculkin/Documents/Software Development/Semester Three/Object Oriented Programming/Rider Projects/Week5Assessment/Week5AssessmentTaskThree/IBANs.txt";
+string[] ibans = File.ReadAllLines(filePath);
+
+char[] iban = new char[22];
+string? bankCode = null;
+string? sortCode = null;
+string? accNumber = null;
+
+
+// Example: IE25ULSB 996622 12345678
+// Main
+for (int i = 0; i < 2; i++)
+{
+    iban = ibans[i].ToCharArray();
+// Bank Code
+    for (int j = 0; i < 8; i++)
+    {
+        bankCode = bankCode + iban[j];
+    }
+// Sort Code
+    for (int j = 8; i < 14; i++)
+    {
+        sortCode += iban[j];
+    }
+
+// Account Number
+    for (int j = 14; i < 22; i++)
+    {
+        accNumber += iban[j];
+    }
+}
+
