94d7e3c07d006b93cd2b59d0f69f01510fea4748
CulkinR <99401818+CulkinR@users.noreply.github.com>
2022-10-20 11:22:57 +0100

    Task 5 - Initial Commit

---
+++ Week5AssessmentTaskFour/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Vars
+// 2D Arary
+double[,] rainfall = new double[,] { {6.9, 7.1, 7.2}, {6.1, 6.4, 6.3}, {6.5, 6.7, 6.9} };
